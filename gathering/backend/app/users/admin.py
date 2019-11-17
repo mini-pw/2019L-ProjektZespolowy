40814a2f9eda118c3c0e9891a785cc3df95ee6cf
Ostrzyciel <ostrzycielnozyczek@gmail.com>
2019-11-17 18:27:08 +0100

    Add missing import

--- gathering/backend/app/users/admin.py
+++ gathering/backend/app/users/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.models import User
+from django.contrib.auth.forms import AdminPasswordChangeForm
 
 from publications.models import Annotation
 
