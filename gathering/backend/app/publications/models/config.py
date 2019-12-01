a3fc1e9376fce0916ade1a13aaf73ec0ba577608
Ostrzyciel nożyczek <ostrzycielnozyczek@gmail.com>
2019-12-01 14:19:56 +0100

    Add admin panels for config

--- gathering/backend/app/publications/models/config.py
+++ gathering/backend/app/publications/models/config.py
@@ -3,7 +3,10 @@ from django.db import models
 class ObjectType(models.Model):
     name = models.CharField(max_length=512)
     key = models.CharField(max_length=512)
-    parent_type = models.ForeignKey('ObjectType', null=True, on_delete=models.PROTECT)
+    parent_type = models.ForeignKey('ObjectType', null=True, blank=True, on_delete=models.PROTECT)
+
+    def __str__(self):
+        return 'Object type: ' + self.name
 
     class Meta:
         ordering = ['key']
@@ -14,6 +17,9 @@ class SubobjectType(models.Model):
     key = models.CharField(max_length=512)
     valid_on = models.ManyToManyField(ObjectType)
 
+    def __str__(self):
+        return 'Subobject type: ' + self.name
+
     class Meta:
         ordering = ['key']
 
@@ -21,6 +27,9 @@ class SubobjectType(models.Model):
 class AnnotationTag(models.Model):
     name = models.CharField(max_length=512)
     key = models.CharField(max_length=512)
+
+    def __str__(self):
+        return 'Annotation tag: ' + self.name
 
     class Meta:
         ordering = ['key']
