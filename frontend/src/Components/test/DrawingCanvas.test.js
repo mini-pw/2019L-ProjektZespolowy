9182089585b0fb6ed6faa8997acd37cd42556a1c
kowe <weronika.iga.kowalska@gmail.com>
2019-12-02 19:01:37 +0100

    Fix tests

--- frontend/src/Components/test/DrawingCanvas.test.js
+++ frontend/src/Components/test/DrawingCanvas.test.js
@@ -9,8 +9,17 @@ describe('DrawingCanvas tests', () => {
         var parent = {pageIndex: 0, annotationIndex: 0, subRegionIndex: null};
         var subRegion = {pageIndex: 0, annotationIndex: 0, subRegionIndex: 0};
         const wrapper = shallow(<DrawingCanvas/>);
-        wrapper.setProps({ selectedAnnotations: [parent, subRegion] });
+        var annotations = [
+            {
+                subRegions: [{data: 'subregion1'}, {data: 'subregion2'}]
+            },
+            {
+                data: 'sample2'
+            }
+        ];
         const instance = wrapper.instance();
+        instance.availableTypes = [];
+        wrapper.setProps({ selectedAnnotations: [parent, subRegion], annotations:  annotations});
         var result = instance.selectedRects();
         expect(result.length).toBe(2);
         expect(result[0]).toBe("rect0");
@@ -28,6 +37,85 @@ describe('DrawingCanvas tests', () => {
     it('formatTypes => returned string correct', () => {
         const wrapper = shallow(<DrawingCanvas/>);
         const instance = wrapper.instance();
+        instance.availableTypes = 
+        [
+          {
+            name: "Plot", 
+            value: "plot",
+            subtypes: [
+              {name: "Title", value: "title", isTextAnnotation: true},
+              {name: "X axis", value: "x_axis", isTextAnnotation: false, orientation: 'horizontal'},
+              {name: "Title of x axis", value: "x_axis_title", isTextAnnotation: true},
+              {name: "Y axis", value: "y_axis", isTextAnnotation: false, orientation: 'vertical'},
+              {name: "Title of y axis", value: "y_axis_title", isTextAnnotation: true},
+              {name: "Text annotation", value: "text_annotation", isTextAnnotation: true}
+            ]
+          },
+          {
+            name: "Chart", 
+            value: "chart",
+            parent: "plot"
+          },
+          {
+            name: "Linear plot", 
+            value: "linear_plot",
+            parent: "plot"
+          },
+          {
+            name: "Pie chart", 
+            value: "pie_chart",
+            parent: "plot"
+          },
+          {
+            name: "Dot plot", 
+            value: "dot_plot",
+            parent: "plot"
+          },
+          {
+            name: "Column plot", 
+            value: "column_plot",
+            parent: "plot"
+          },
+          {
+            name: "Box plot", 
+            value: "box_plot",
+            parent: "plot"
+          },
+          {
+            name: "Other plot", 
+            value: "other_plot",
+            parent: "plot"
+          },
+          {
+            name: "Table", 
+            value: "table",
+            subtypes: [
+              {name: "Cell", value: "cell", isTextAnnotation: true},
+              {name: "Title", value: "title", isTextAnnotation: true},
+              {name: "Row", value: "row", isTextAnnotation: false, orientation: 'horizontal'},
+              {name: "Row Title", value: "row_title", isTextAnnotation: true},
+              {name: "Column", value: "column", isTextAnnotation: false, orientation: 'vertical'},
+              {name: "Column Title", value: "column_title", isTextAnnotation: true},
+              {name: "Text annotation", value: "text_annotation", isTextAnnotation: true}
+            ]
+          },
+          {
+            name: "ChaTa reference", 
+            value: "chata_reference"
+          },
+          {
+            name: "Image", 
+            value: "image"
+          },
+          {
+            name: "Algorithm", 
+            value: "algorithm"
+          },
+          {
+            name: "Diagram", 
+            value: "diagram"
+          }
+        ];
         var result = instance.formatTypes(['cell', 'chart']);
         expect(result).toBe('Cell,Chart');
     });
